bfed67100a3a00b44d6739e12c716ce4151c8612
Hilal Mufti <hmufti@cs.washington.edu>
2024-08-13 18:46:17 -0700

    add - joint training bash script

---
+++ gen-run-train-joint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <arms> <gpu> <epochs>"
+    exit 1
+fi
+
+ARMS=$1
+GPU=$2
+EPOCHS=$3
+FILE="train-scripts/run-train-joint-A$ARMS-E$EPOCHS-$GPU.sh"
+
+
+
+cat << EOF > $FILE
+#!/bin/bash
+#SBATCH -N1
+#SBATCH --gpus=$GPU:1
+#SBATCH -c 32
+#SBATCH --mem=32G
+#SBATCH -p celltypes
+#SBATCH -o mmidas-logs/mmidas_%j.out
+#SBATCH -e mmidas-logs/mmidas_%j.err
+#SBATCH --time=96:00:00
+
+source activate mdist-mmidas
+
+python train.py --n_arm $ARMS --use-wandb --gpus 1 --n_epoch $EPOCHS
+EOF
