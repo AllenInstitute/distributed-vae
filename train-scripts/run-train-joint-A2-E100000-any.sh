ea640f9643cfaf99d00c8f736f19536364540c06
hilalmufti <hmufti@cs.washington.edu>
2024-08-14 23:00:45 -0700

    +update time

--- train-scripts/run-train-joint-A2-E100000-any.sh
+++ train-scripts/run-train-joint-A2-E100000-any.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -N1
-#SBATCH --gpus=v100:1
+#SBATCH --gpus=1
 #SBATCH -c 32
 #SBATCH --mem=32G
 #SBATCH -p celltypes
 #SBATCH -o mmidas-logs/mmidas_%j.out
 #SBATCH -e mmidas-logs/mmidas_%j.err
-#SBATCH --time=96:00:00
+#SBATCH --time=48:00:00
 
 source activate mdist-mmidas
 
