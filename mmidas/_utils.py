439ef95acb619e6c7170b608cfd5c52c6066144c
hilalmufti <hmufti@cs.washington.edu>
2024-10-05 20:36:27 -0700

    Implement confusion_matrix()

--- mmidas/_utils.py
+++ mmidas/_utils.py
@@ -23,3 +23,15 @@ def to_np(x):
 @unstable
 def mk_masks(bias: th.Tensor) -> tuple[np.ndarray, np.ndarray]:
     return np.where(bias.cpu() != 0)[0], np.where(bias.cpu() == 0)[0]
+
+
+@unstable
+# TODO: add type hints that this takes one-hot encoded labels
+# Note, all labels are assumed to be present in both arrays
+def confusion_matrix(labels1, labels2):
+    assert len(labels1) == len(labels2)
+    assert len(labels1.shape) == len(labels2.shape) == 1
+    K1, K2 = len(np.unique(labels1)), len(np.unique(labels2))
+    matrix = np.zeros((K1, K2), dtype=int)
+    np.add.at(matrix, (labels1, labels2), 1)
+    return matrix
